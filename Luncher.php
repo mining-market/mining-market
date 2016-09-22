de7fbf0b67b4e375a89b3937644b0231c9a0241a
miningmarket <fsa.akbar@gmail.com>
2016-09-22 12:46:10 +0330

    Update Luncher.php

--- Luncher.php
+++ Luncher.php
@@ -1,5 +1,5 @@
 <?php
-define('BOT_TOKEN', '289112299:AAE8YCI6clrwaXQKbNW4ZuyBk-hJLeod6Qw');
+define('BOT_TOKEN', 'jhghjkjk');
 define('API_URL', 'https://api.telegram.org/bot'.BOT_TOKEN.'/');
 
 function apiRequestWebhook($method, $parameters) {
@@ -115,7 +115,7 @@ function processMessage($message) {
   // process incoming message
   $boolean = file_get_contents('booleans.txt');
   $booleans= explode("\n",$boolean);
-  $admin = 201081978;
+  $admin = 20009998778;
   $message_id = $message['message_id'];
   $rpto = $message['reply_to_message']['forward_from']['id'];
   $chat_id = $message['chat']['id'];
